fa18e4fbcff95e93ad554b0bee015010acd645c2
Alzubi <abdulmalik.alzubi@siemens.com>
2024-08-16 09:01:22 +0200

    Deine Commit-Nachricht

---
+++ prozeduraleProgrammierung_1/main.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    int test;
+    //scanf("%i", &test); // Verwende weiterhin scanf
+    return 0;
+}
